7a61007d6a1bb8eccb59e51e6cca493c0164eb76
starlit-nox <kdkelly.1998@gmail.com>
2023-03-31 20:32:56 -0500

    lab-13 complete

--- duck.js
+++ duck.js
@@ -1,17 +1,20 @@
 `use strict`;
 
+// variables that select elements in the html
 let productContainer = document.querySelector("section");
 let resultButton = document.querySelector("section + div");
 let image1 = document.querySelector("section img:first-child");
 let image2 = document.querySelector("section img:nth-child(2)");
 let image3 = document.querySelector("section img:nth-child(3)");
 
+// this sets the beginning click for the clicking vote
 let click = 0;
+// this sets the maxium clicks entirely
 let maxClicksAllowed = 25;
 
 const views = {};
 
-// State object holds the holds the current state of the application (all existing products)
+// holds the current state of the app and products
 const state = {
   allproductsArray: [],
 };
@@ -25,6 +28,7 @@ function product(path, name) {
   this.click = 0;
 }
 
+// this is the array for the product img files
 const imageFiles = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
 // image counter
@@ -35,44 +39,52 @@ function getRandomNumber(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-// call the getRandomNumber
+// this calls the getRandomNumber function to generate a random number to randomly select an img
+// this is also getRandomNumber for product 1
 function renderProducts() {
   const product1Image = getRandomNumber(imageFiles);
   image1.src = `img/${product1Image}`;
-    if (views[product1Image]) {
-      views[product1Image]++;
-    } else {
-      views[product1Image] = 1;
-    }
-  
-    const product2Image = getRandomNumber(imageFiles);
-    image2.src = `img/${product2Image}`;
-    if (views[product2Image]) {
-      views[product2Image]++;
-    } else {
-      views[product2Image] = 1;
-    }
-  
-    const product3Image = getRandomNumber(imageFiles);
-    image3.src = `img/${product3Image}`;
-    if (views[product3Image]) {
-      views[product3Image]++;
-    } else {
-      views[product3Image] = 1;
-    }
+  if (views[product1Image]) {
+    views[product1Image]++;
+  } else {
+    views[product1Image] = 1;
   }
 
-  
+  // this is the getRandomNumber for product 2
+  const product2Image = getRandomNumber(imageFiles);
+  image2.src = `img/${product2Image}`;
+  if (views[product2Image]) {
+    views[product2Image]++;
+  } else {
+    views[product2Image] = 1;
+  }
+
+  // this is the getRandomNumber for product 3
+  const product3Image = getRandomNumber(imageFiles);
+  image3.src = `img/${product3Image}`;
+  if (views[product3Image]) {
+    views[product3Image]++;
+  } else {
+    views[product3Image] = 1;
+  }
+}
+
+
 // this puts thenm in the html
 const product1Span = document.getElementById('image1');
 const product2Span = document.getElementById('image2');
 const product3Span = document.getElementById('image3');
 
+
+// this is the function for the event clicker w/ an alert
+// alert happens if user clicks off the img while voting is in session
 function handleProductClick(event) {
   if (event.target === productContainer) {
     alert("Please click on an image");
   }
   click++;
+  localStorage.setItem("state", JSON.stringify(state));
+
 
   let clickProduct = event.target.alt;
   for (let i = 0; i < state.allproductsArray.length; i++) {
@@ -83,8 +95,9 @@ function handleProductClick(event) {
   }
   if (click === maxClicksAllowed) {
     productContainer.removeEventListener("click", handleProductClick);
-    // give the button an event lister and styles so the user
-    // knows its an active button:
+    // the above code removes the even listener after 25 images has been clicked
+
+    // this gives the results an event listener for clicks when clicked
     resultButton.addEventListener("click", renderResults);
     resultButton.className = "clicks-allowed";
     productContainer.className = "no-voting";
@@ -93,6 +106,9 @@ function handleProductClick(event) {
   }
 }
 
+// this renders proucts results and displays them on the html
+// formerly this displayed them in a li, but the feature was removed
+// they now are displayed as a bar graph when myChart canvas was added
 function renderResults() {
   let ul = document.querySelector("ul");
   for (let i = 0; i < state.allproductsArray.length; i++) {
@@ -114,19 +130,19 @@ function showResults() {
 
 productContainer.addEventListener("click", handleProductClick);
 
-// Event listener for the View Results button
+// this is the event listener for results
 const viewResultsBtn = document.querySelector('.view-results-btn');
 viewResultsBtn.addEventListener('click', showResults);
 
 function printChart() {
-  // Create an array of labels and data from the imageCount object
+  // creates an array of labels and data from the imageCount object
   const labels = Object.keys(views);
   const data = Object.values(views);
 
-  // Get the canvas element for the chart
+  // get the canvas element for the chart
   const canvas = document.getElementById('myChart');
 
-  // Create the chart using Chart.js
+  // create the chart using Chart.js
   const chart = new Chart(canvas, {
     type: 'bar',
     data: {
@@ -135,7 +151,7 @@ function printChart() {
         label: 'Image Votes',
         data: data,
         backgroundColor: [
-            "#000000", "#111111", "#222222", "#333333", "#444444", "#555555", "#666666", "#777777", "#888888", "#999999", "#AAAAAA", "#BBBBBB", "#CCCCCC", "#DDDDDD", "#EEEEEE",
+          "#000000", "#111111", "#222222", "#333333", "#444444", "#555555", "#666666", "#777777", "#888888", "#999999", "#AAAAAA", "#BBBBBB", "#CCCCCC", "#DDDDDD", "#EEEEEE",
 
         ]
       }]
@@ -151,7 +167,25 @@ function printChart() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const viewResultsBtn = document.querySelector('.view-results-btn');
   viewResultsBtn.addEventListener('click', showResults);
 });
+
+// localstorage function
+
+// instructions: Store the products array into local storage as a formatted JSON string
+// Retrieve the products array from local storage and then utilize the JSON.Parse() 
+// function. Remember, if your constructor utilizes prototype methods, you will have to 
+// send each item in the array back through the constructor function.
+
+localStorage.setItem('product', JSON.stringify(Object));
+
+let myObj = JSON.parse(localStorage.getItem('product'));
+
+localStorage.imageFiles = JSON.stringify(product);
+
+let myJSON = JSON.parse(localStorage.imageFiles);
+
+let string = localStorage.setItem('product');
+localStorage.setItem('product', str);
